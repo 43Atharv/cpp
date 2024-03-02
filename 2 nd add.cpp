7d285a1c6951c2688cadc99aeb15a10a8ed9bbeb
Atharv43 <154349297+Athara43@users.noreply.github.com>
2024-03-02 18:10:11 +0530

    Add files via upload

---
+++ 2 nd add.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	int c;
+    cout<<"enter the value of A and B"<<endl;
+    cin>>a>>b;
+    
+    
+    cout<<"a+b="<<a+b<<endl;
+    cout<<"a-b="<<a-b<<endl;
+    cout<<"a*b="<<a*b<<endl;
+    cout<<"a/b="<<a/b<<endl;
+    cout<<"a%b="<<a%b<<endl;
+    return 0;
+}
