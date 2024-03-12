05129bdda34d752800e2085da37895619211c47f
Atharv43 <154349297+Athara43@users.noreply.github.com>
2024-03-12 18:29:11 +0530

    Add files via upload

---
+++ big brother if else.cpp
@@ -0,0 +1,19 @@
+//nsetd loop
+#include<iostream>
+using namespace std;
+int main()
+{
+	int atharv,manish;
+	cout<<"enter the element A and B"<<endl;
+	cin>>atharv>>manish;
+	if(atharv>manish)
+	{
+		cout<<"Atharv is a big brother"<<endl;
+	}
+	else
+	{
+		cout<<"manish is big brother"<<endl;
+	}
+	
+	return 0;
+}
