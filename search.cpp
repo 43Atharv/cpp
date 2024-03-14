70bba677363c6a370172c954741dc5ab62edbbba
Atharv43 <154349297+Athara43@users.noreply.github.com>
2024-03-14 21:07:04 +0530

    Add files via upload

---
+++ search.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int low,high,mid;
+	cout<<"enter the value of low and high"<<endl;
+	cin>>low>>high;
+	do
+	{
+		mid=(high+low)/2;
+	}
+	
+	
+	if{
+		
+		a(x)<mid;
+		then mid=high-1;
+	      }
+	      else
+	      {
+	      	mid=low-1;
+		  }
+		  
+		
+	
+}
