6e763a7265a454de0aeb5574bdec39a1d5e76c8b
Atharv Chougule <154349297+43Atharv@users.noreply.github.com>
2024-04-30 10:29:27 +0530

    Add files via upload

--- function overloading.cpp
+++ function overloading.cpp
@@ -1,22 +1,27 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
- 
- 
-void add(int a, int b)
+int add(int a,int b)
 {
-  cout << "sum = " << (a + b);
+	return a+b;
 }
- 
-void add(double a, double b)
+int add(int a,int b,int c)
 {
-    cout << endl << "sum = " << (a + b);
+	return a+b+c;
 }
- 
-// Driver code
+float add(float a,float b)
+{
+	return a+b;
+}
 int main()
 {
-    add(10, 2);
-    add(5.3, 6.2);
- 
-    return 0;
+	int a,b,c;
+	float x,y;
+	cout<<"enter the integer value of ab"<<endl;
+	cin>>a>>b>>c;
+	cout<<"enter the floar value if x and y"<<endl;
+	cin>>x>>y;
+	cout<<"addition of two integer value is:"<<add(a,b)<<endl;
+	cout<<"addition of three intger value is:"<<add(a,b,c)<<endl;
+	cout<<"addition of two float value is :"<<add(x,y)<<endl;
+	return 0;
 }
