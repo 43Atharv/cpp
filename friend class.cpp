d2b278e15f6e99d21d009adcde9b52a5e8b8355e
Atharv Chougule <154349297+43Atharv@users.noreply.github.com>
2024-05-01 10:38:45 +0530

    Add files via upload

---
+++ friend class.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class a
+{
+	public:
+		int x=10;
+		void setx(int i)
+		{
+			x=i;
+		}
+		friend class b;
+};
+class b
+{
+	public:
+		void disp(a&a1)
+		{
+			cout<<a1.x;
+		}
+};
+int main()
+{
+	a a1;
+	b b1;
+	b1.disp(a1);
+	return 0;
+}
